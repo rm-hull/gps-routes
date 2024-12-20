3521203a5d7e33ecc3115b605bdd5d70e1667faa
Richard Hull <rm_hull@yahoo.co.uk>
2024-12-20 21:30:50 +0000

    Only load tanstack router devtools in non-production mode

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,23 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import React, { Suspense } from "react";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        }))
+      );
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools initialIsOpen={false} />
+      <Suspense>
+        <TanStackRouterDevtools initialIsOpen={false} />
+      </Suspense>
     </>
   ),
 });
