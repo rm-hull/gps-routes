751f1506b001343b23d5249d7373a1f1050c78bc
Richard Hull <rm_hull@yahoo.co.uk>
2026-02-06 21:16:17 +0000

    perf: memoize JSON stringification
    Memoize the JSON stringification of the `data` prop using `useMemo`.
    This prevents redundant computations on every component render when the
    source data has not changed.

--- src/components/JsonCodeBlock.tsx
+++ src/components/JsonCodeBlock.tsx
@@ -1,6 +1,7 @@
 import { CodeBlock, Float, IconButton } from "@chakra-ui/react";
 import { HighlighterGeneric } from "shiki";
 import { createShikiAdapter } from "@chakra-ui/react";
+import { useMemo } from "react";
 import { useColorMode } from "./ui/color-mode";
 
 const shikiAdapter = createShikiAdapter<HighlighterGeneric<any, any>>({
@@ -20,13 +21,14 @@ type JsonCodeBlockProps = {
 
 export function JsonCodeBlock({ data }: JsonCodeBlockProps) {
   const { colorMode } = useColorMode();
+  const formatted = useMemo(() => JSON.stringify(data, null, 2), [data]);
 
   return (
     <CodeBlock.AdapterProvider value={shikiAdapter}>
       <CodeBlock.Root
         size="sm"
         opacity={0.7}
-        code={JSON.stringify(data, null, 2)}
+        code={formatted}
         language="json"
         meta={{
           showLineNumbers: true,
